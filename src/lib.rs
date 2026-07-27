2752651341852a4fe6ce6b89b5c1e4cf18beb118
khaosduke <khaosduke@gmail.com>
2026-07-27 17:07:57 -0400

    start of public facing api

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,26 @@
+use reqwest::Client;
+use reqwest::Response;
+
 mod request;
 mod constants;
 mod validators;
 
 pub mod errors;
-
 pub use request::build_get_request;
 
+pub struct RxNormApi {
+    client: reqwest::Client,
+}
+
+impl RxNormApi {
+    pub fn new() -> Self {
+        Self {
+            client: reqwest::Client::new(),
+        }
+    }
+
+    //pub fn get(function:&str, options&HashMap<&str,&str>) -> reqwest::Response {
+        
+    //}
+}
 
