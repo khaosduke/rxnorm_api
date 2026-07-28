92d2897f3dab7ec6aede889b7b72b69be08f5422
khaosduke <khaosduke@gmail.com>
2026-07-27 22:58:43 -0400

    first endpoint - not complete

--- src/endpoints/find.rs
+++ src/endpoints/find.rs
@@ -1,10 +1,19 @@
 use crate::{errors::RxNormError, RxNormApi};
+use std::collections::HashMap;
+use rxnorm_api::validators::verify_options_hash;
 
 impl RxNormApi {
      pub async fn find_rxcui_by_string(
          &self,
          name: &str,
+         options: &HashMap<&str,&str>,
      ) -> Result<reqwest::Response, RxNormError> {
+
+        let mut working_options = options.clone();
+        let _ = working_options.insert("name",name);
+
+        verify_options_hash("findRxcuiByString",&working_options)?;
+        
 
      }
 //
